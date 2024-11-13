ad0d2adf5b9fcf9be14c966d24c12640abc614b3
Hernan Liseras <herchi@Hernans-MacBook-Pro.local>
2024-11-13 11:16:43 -0300

    without version :|

--- packages/solv/src/cli/get/snapshot.ts
+++ packages/solv/src/cli/get/snapshot.ts
@@ -5,11 +5,10 @@ export const getSnapshot = (
   isTest = false,
   minDownloadSpeed = '45',
   ledgerPath = MT_PATHS.LEDGER,
-  snapshotPath = `${ledgerPath}/snapshot`,
-  version: string
+  snapshotPath = `${ledgerPath}/snapshot`
 ) => {
   try {
-    let cmd = `docker run -it --rm -v ${ledgerPath}:${snapshotPath} --user $(id -u):$(id -g) c29r3/solana-snapshot-finder:latest --snapshot_path ${snapshotPath} --min_download_speed ${minDownloadSpeed} --version ${version}`
+    let cmd = `docker run -it --rm -v ${ledgerPath}:${snapshotPath} --user $(id -u):$(id -g) c29r3/solana-snapshot-finder:latest --snapshot_path ${snapshotPath} --min_download_speed ${minDownloadSpeed}`
     if (isTest) {
       spawnSync(
         `wget --trust-server-names https://snapshots.avorio.network/testnet/snapshot.tar.bz2 https://snapshots.avorio.network/testnet/incremental-snapshot.tar.bz2`,
